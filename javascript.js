4357c04dca0a886bc5d31e5898d4dd1ddd4c136f
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-09-01 00:21:07 -0400

    Add hovering function to boxes
    When a mouseover event occurs over a box it will be
    highlighted black and stay that color to simulate a
    real pen.

--- javascript.js
+++ javascript.js
@@ -4,9 +4,17 @@ window.addEventListener("resize", addBoxes);
 
 window.dispatchEvent(new Event("resize"));
 
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((currentValue) => {
+    currentValue.addEventListener("mouseover", () => {
+        currentValue.style.backgroundColor = "black";
+    })
+})
+
 function addBoxes() {
     let pageWidth = window.innerWidth;
-    let boxWidth = parseFloat(pageWidth)/17;
+    let boxWidth = parseFloat(pageWidth)/17; // dividing by 16 does not distribute enough space to have 16 boxes so we use 17 instead
     let pageHeight = window.innerHeight;
     let boxHeight = parseFloat(pageHeight)/17;
 
