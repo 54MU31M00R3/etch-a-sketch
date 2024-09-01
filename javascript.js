6df11e9b27692d46f1f33923389150d7dcfbe653
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-09-01 00:55:17 -0400

    Add button to change grid layout
    Allows user to select number of boxes for the grid
    between 0 to 100

--- javascript.js
+++ javascript.js
@@ -1,25 +1,30 @@
+let gridSize = 16;
 const grid = document.querySelector("#container");
 
 window.addEventListener("resize", addBoxes);
 
 window.dispatchEvent(new Event("resize"));
 
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach((currentValue) => {
-    currentValue.addEventListener("mouseover", () => {
-        currentValue.style.backgroundColor = "black";
-    })
+const newGridBtn = document.querySelector("#grid");
+newGridBtn.addEventListener("click", () => {
+    do {
+        gridSize = parseFloat(prompt("Please enter a grid size less than 100."));
+    } while (!(gridSize < 100 && gridSize > 0))
+    
+        while (grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+    addBoxes()
 })
 
 function addBoxes() {
     let pageWidth = window.innerWidth;
-    let boxWidth = parseFloat(pageWidth)/17; // dividing by 16 does not distribute enough space to have 16 boxes so we use 17 instead
+    let boxWidth = parseFloat(pageWidth)/(gridSize + 1); // dividing by the base number does not distribute enough space so we use +1 instead
     let pageHeight = window.innerHeight;
-    let boxHeight = parseFloat(pageHeight)/17;
+    let boxHeight = parseFloat(pageHeight)/(gridSize + 1); 
 
-    for (let i = 0; i < 16; i++){
-        for (let j = 0; j < 16; j++){
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
         const box = document.createElement("div");
         box.classList.add("box");
         grid.appendChild(box);
@@ -29,5 +34,10 @@ function addBoxes() {
     boxes.forEach((currentValue) =>{
         currentValue.style.minWidth = boxWidth + "px";
         currentValue.style.minHeight = boxHeight + "px";
+    })
+    boxes.forEach((currentValue) => {
+        currentValue.addEventListener("mouseover", () => {
+            currentValue.style.backgroundColor = "black";
+        })
     })
 }
